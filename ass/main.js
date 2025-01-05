9ddf35c4891813c68f585ec49061313514d3a7e6
Victor Alpert <youremail@example.com>
2025-01-05 15:58:05 -0800

    Trying some crazy techbro stuff rn dude.

---
+++ ass/main.js
@@ -0,0 +1,33 @@
+let dvd = document.getElementById('dvd');
+let interval_id;
+let x_incr = 1;
+let y_incr = 1;
+
+function init() {
+  dvd.style.position = 'absolute';
+  setInterval(frame, 1);
+}
+
+function handle_collision() {
+  let dvd_height = dvd.offsetHeight;
+  let dvd_width = dvd.offsetWidth;
+  let left = dvd.offsetLeft;
+  let top = dvd.offsetTop;
+  let win_height = window.innerHeight;
+  let win_width = window.innerWidth;
+
+  if (left <= 0 || left + dvd_width >= win_width) {
+    x_incr = ~x_incr + 1;
+  }
+  if (top <= 0 || top + dvd_height >= win_height) {
+    y_incr = ~y_incr + 1;
+  }
+}
+
+function frame() {
+  handle_collision();
+  dvd.style.top = dvd.offsetTop + y_incr;
+  dvd.style.left = dvd.offsetLeft + x_incr;
+}
+
+init();
